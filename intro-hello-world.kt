d8c0c8984be1a63c67d58bf80e6c655770152528
Gediel Durães de Almeida <95309923+Gediel99@users.noreply.github.com>
2022-12-25 10:35:12 -0300

    Adiciona Hello, World

---
+++ intro-hello-world.kt
@@ -0,0 +1,5 @@
+package me.dio.helloworld     //1. Definição do pacote (algo muito importante para organização de código)
+
+fun main() {                    //2. Ponto de entrada de um código Kotlin, definido pela função [main]
+    println("Hello, World!");   //3. Imprime na saída padrão um texto, pulando linha.
+}
