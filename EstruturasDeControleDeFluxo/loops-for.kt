8c962f3d4ab53060a80d87c1023f723fe602e0c9
Gediel Durães de Almeida <95309923+Gediel99@users.noreply.github.com>
2022-12-27 13:34:18 -0300

    Adiciona assunto sobre Loops: For

---
+++ EstruturasDeControleDeFluxo/loops-for.kt
@@ -0,0 +1,8 @@
+package EstruturasDeControleDeFluxo
+
+fun main(){
+    val bolos = listOf("cenoura com chocolate","ninho","chocolate")           //1. Adiciona uma lista de bolos
+    for (i in bolos) {                                                        //2. Atribui os elementos contidos na lista de bolos a cada iteração em i.
+        println("Gediel, temos o bolo de $i!")
+    }
+}
