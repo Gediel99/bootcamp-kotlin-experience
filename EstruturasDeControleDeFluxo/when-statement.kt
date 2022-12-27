67480a0c7a31c4fb7b2317a8dd5764e969dfc9c3
Gediel Durães de Almeida <95309923+Gediel99@users.noreply.github.com>
2022-12-27 10:08:26 -0300

    Adiciona arquivo sobre Estruturas de Controle de Fluxo

---
+++ EstruturasDeControleDeFluxo/when-statement.kt
@@ -0,0 +1,21 @@
+/*
+    O When pode ser usado no lugar de estruturas de switch, são mais limpas e flexíveis. Elas podem ser usadas como uma declaração ou como uma expressão.
+*/
+fun main(){
+    cases("Hello")
+    cases(1)
+    cases(0L)
+    cases(MyClass())
+    cases("hello")
+}
+fun cases(obj: Any){                                                //1. Declara uma função "cases" que recebe Any (algo que recebe qualquer coisa) de parâmetro;
+    when (obj) {                                                    //2. Quando o obj for tal caso, ele imprimirá.
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long")
+        !is String -> println("Not a string")
+        else -> println("Unknown")
+    }
+}
+
+class MyClass
